1d3dfdbd08b41fae5f940fc285c6ef3b182e0ad4
Roselyn Chin <roseychin5@gmail.com>
2021-08-08 00:58:46 -0700

    Initial Commit

---
+++ ToDoListAppGTests/ToDoListAppGTests.swift
@@ -0,0 +1,33 @@
+//
+//  ToDoListAppGTests.swift
+//  ToDoListAppGTests
+//
+//  Created by  Scholar on 8/8/21.
+//
+
+import XCTest
+@testable import ToDoListAppG
+
+class ToDoListAppGTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
